2c391d869e9c7a141f4f1ae320ea5d61f0df4655
Wefour <we46584@gmail.com>
2025-03-11 16:07:15 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// Fonction générique pour gérer les modals
+function setupModal(modalId, btnId, closeId) {
+    var modal = document.getElementById(modalId);
+    var btn = document.getElementById(btnId);
+    var span = document.getElementById(closeId);
+
+    // Ouvrir le modal
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // Fermer le modal quand on clique sur "X"
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // Fermer le modal si l'utilisateur clique en dehors du contenu
+    window.addEventListener("click", function(event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    });
+}
+0
+// Initialisation des modals
+setupModal("modalHome", "Home", "closeModalHome");
+setupModal("modalBts", "Bts", "closeModalBts");
+setupModal("modalVeille", "Veille-techno", "closeModalVeille");
+setupModal("modalProjects", "Projects", "closeModalProjects");
+
+
+
